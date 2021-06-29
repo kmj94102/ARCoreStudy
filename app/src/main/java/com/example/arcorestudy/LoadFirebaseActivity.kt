02a1531dd0d895fd1c7b3574c1a3713614f00689
minjae <kmj94102@gmail.com>
2021-06-29 16:58:30 +0900

    이름표 추가

--- app/src/main/java/com/example/arcorestudy/LoadFirebaseActivity.kt
+++ app/src/main/java/com/example/arcorestudy/LoadFirebaseActivity.kt
@@ -3,11 +3,15 @@ package com.example.arcorestudy
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 import com.example.arcorestudy.databinding.ActivityLoadFirebaseBinding
 import com.google.ar.sceneform.AnchorNode
 import com.google.ar.sceneform.assets.RenderableSource
+import com.google.ar.sceneform.math.Vector3
 import com.google.ar.sceneform.rendering.ModelRenderable
+import com.google.ar.sceneform.rendering.ViewRenderable
 import com.google.ar.sceneform.ux.ArFragment
+import com.google.ar.sceneform.ux.TransformableNode
 import com.google.firebase.FirebaseApp
 import com.google.firebase.ktx.Firebase
 import com.google.firebase.storage.ktx.storage
@@ -19,6 +23,7 @@ class LoadFirebaseActivity : AppCompatActivity() {
 
     private val binding : ActivityLoadFirebaseBinding by lazy { ActivityLoadFirebaseBinding.inflate(layoutInflater) }
     private lateinit var renderable : ModelRenderable
+    private lateinit var arFragment: ArFragment
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,7 +33,7 @@ class LoadFirebaseActivity : AppCompatActivity() {
 
         val modelRef = Firebase.storage.reference.child("out.glb")
 
-        val arFragment = supportFragmentManager.findFragmentById(R.id.arFragment) as ArFragment
+        arFragment = supportFragmentManager.findFragmentById(R.id.arFragment) as ArFragment
 
         binding.downloadBtn.setOnClickListener {
             try {
@@ -44,8 +49,16 @@ class LoadFirebaseActivity : AppCompatActivity() {
 
         arFragment.setOnTapArPlaneListener{ hitResult, _, _ ->
             val anchorNode =AnchorNode(hitResult.createAnchor())
-            anchorNode.renderable = renderable
-            arFragment.arSceneView.scene.addChild(anchorNode)
+            anchorNode.setParent(arFragment.arSceneView.scene)
+//            anchorNode.renderable = renderable
+            val transFromAbleNode = TransformableNode(arFragment.transformationSystem)
+            transFromAbleNode.setParent(anchorNode)
+            transFromAbleNode.renderable = renderable
+            transFromAbleNode.select()
+
+//            arFragment.arSceneView.scene.addChild(anchorNode)
+
+            addName(anchorNode, transFromAbleNode, "고우스트")
         }
 
     }
@@ -66,6 +79,25 @@ class LoadFirebaseActivity : AppCompatActivity() {
             .thenAccept{ modelRenderable ->
                 toast("Model built")
                 renderable = modelRenderable
+            }
+    }
+
+    private fun addName(anchorNode: AnchorNode, node: TransformableNode, name: String) {
+        ViewRenderable.builder().setView(this, R.layout.name_card)
+            .build()
+            .thenAccept { viewRenderable ->
+                val nameView = TransformableNode(arFragment.transformationSystem)
+                nameView.localPosition = Vector3(0f, node.localPosition.y + 0.5f, 0f)
+                nameView.setParent(anchorNode)
+                nameView.renderable = viewRenderable
+                nameView.select()
+
+                val txtName = viewRenderable.view as TextView
+                txtName.text = name
+                txtName.setOnClickListener {
+                    anchorNode.setParent(null)
+
+                }
             }
     }
 
