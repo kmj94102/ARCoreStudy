f911cae8bc176f974768d37c40c1437f020e61f5
kmj94102 <60861383+kmj94102@users.noreply.github.com>
2021-07-12 22:04:09 +0900

    Animation Test

--- app/src/main/java/com/example/arcorestudy/AnimationActivity.kt
+++ app/src/main/java/com/example/arcorestudy/AnimationActivity.kt
@@ -6,13 +6,20 @@ import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.MotionEvent
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.core.content.ContextCompat
 import com.example.arcorestudy.databinding.ActivityAnimationBinding
+import com.google.ar.core.Anchor
+import com.google.ar.core.HitResult
+import com.google.ar.core.Plane
 import com.google.ar.sceneform.AnchorNode
+import com.google.ar.sceneform.SkeletonNode
 import com.google.ar.sceneform.animation.ModelAnimator
 import com.google.ar.sceneform.assets.RenderableSource
 import com.google.ar.sceneform.rendering.ModelRenderable
+import com.google.ar.sceneform.rendering.Renderable
 import com.google.ar.sceneform.ux.ArFragment
 import com.google.ar.sceneform.ux.TransformableNode
 import com.google.firebase.FirebaseApp
@@ -24,103 +31,74 @@ import java.io.File
 
 class AnimationActivity : AppCompatActivity() {
 
-    private val binding : ActivityAnimationBinding by lazy { ActivityAnimationBinding.inflate(layoutInflater) }
-
-    private lateinit var arFragment : ArFragment
-    private var anchorNode : AnchorNode? = null
-    private var animator : ModelAnimator? = null
-    private var nextAnimation : Int = 0
-    private var animationCrab : ModelRenderable? = null
-
-    private lateinit var transformableNode: TransformableNode
-    private var reference: StorageReference = Firebase.storage.reference
+    private val binding : ActivityAnimationBinding by lazy{
+        ActivityAnimationBinding.inflate(layoutInflater)
+    }
+    lateinit var arFragment: ArFragment
+    private lateinit var model: Uri
+    private var renderable: ModelRenderable? = null
+    private var animator: ModelAnimator? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
-        FirebaseApp.initializeApp(this)
-
         arFragment = supportFragmentManager.findFragmentById(R.id.sceneFormFragment) as ArFragment
-        arFragment.setOnTapArPlaneListener { hitResult, plane, motionEvent ->
-            animationCrab ?: return@setOnTapArPlaneListener
+        model = Uri.parse("model_fight.sfb")
 
-            // create the Anchor
+        arFragment.setOnTapArPlaneListener { hitResult: HitResult, plane: Plane, motionEvent: MotionEvent ->
+            if (plane.type != Plane.Type.HORIZONTAL_UPWARD_FACING) {
+                return@setOnTapArPlaneListener
+            }
             val anchor = hitResult.createAnchor()
-            if(anchorNode == null){
-                anchorNode = AnchorNode(anchor)
-                anchorNode!!.setParent(arFragment.arSceneView.scene)
-
-                transformableNode = TransformableNode(arFragment.transformationSystem)
-                // Scale model
-                transformableNode.scaleController.minScale = 0.09f
-                transformableNode.scaleController.maxScale = 0.1f
-                transformableNode.setParent(anchorNode)
-                transformableNode.renderable = animationCrab
-
-            }
+            placeObject(arFragment, anchor, model)
         }
 
-        // add frame update to control state of button
-        arFragment.arSceneView.scene.addOnUpdateListener {
-            if(anchorNode == null){
-                if(binding.btnAnim.isEnabled){
-                    binding.btnAnim.backgroundTintList = ColorStateList.valueOf(Color.GRAY)
-                    binding.btnAnim.isEnabled = false
-                }
-            }else{
-                if(binding.btnAnim.isEnabled.not()){
-                    binding.btnAnim.backgroundTintList = ContextCompat.getColorStateList(this, R.color.teal_200)
-                    binding.btnAnim.isEnabled = true
-                }
-            }
-        }
-
-        binding.btnAnim.isEnabled = false
-        binding.btnAnim.setOnClickListener {
-            if(animator == null || animator?.isRunning == false){
-                val data = animationCrab?.getAnimationData(nextAnimation)
-                nextAnimation = (nextAnimation+1) % (animationCrab?.animationDataCount ?: 1)
-                animator = ModelAnimator(data, animationCrab)
-                animator?.start()
-            }
-        }
-
-        binding.btnDownload.setOnClickListener {
-            downloadFile()
-        }
-
+        binding.animateKickButton.setOnClickListener { animateModel("Character|Kick") }
+        binding.animateIdleButton.setOnClickListener { animateModel("Character|Idle") }
+        binding.animateBoxingButton.setOnClickListener { animateModel("Character|Boxing") }
     }
 
-    private fun downloadFile(){
-        val reference: StorageReference = Firebase.storage.reference
-        val file = File.createTempFile("clock", "glb")
-
-        reference.child("clock.glb").getFile(file).addOnSuccessListener { task ->
-            buildModel(file)
+    private fun animateModel(name: String) {
+        animator?.let { it ->
+            if (it.isRunning) {
+                it.end()
+            }
+        }
+        renderable?.let { modelRenderable ->
+            val data = modelRenderable.getAnimationData(name)
+            animator = ModelAnimator(data, modelRenderable)
+            animator?.start()
         }
     }
 
-    private fun buildModel(file: File){
-        val renderableSource =
-            RenderableSource
-                .builder()
-                .setSource(this, Uri.parse(file.path), RenderableSource.SourceType.GLB)
-                .setRecenterMode(RenderableSource.RecenterMode.ROOT)
-                .build()
-
-        ModelRenderable
-            .builder()
-            .setSource(this, renderableSource)
-            .setRegistryId(file.path)
+    private fun placeObject(fragment: ArFragment, anchor: Anchor, model: Uri) {
+        ModelRenderable.builder()
+            .setSource(fragment.context, model)
             .build()
-            .thenAccept{ modelRenderable ->
-                toast("다운로드 완료")
-                animationCrab = modelRenderable
+            .thenAccept {
+                renderable = it
+                addToScene(fragment, anchor, it)
             }
             .exceptionally {
-                toast("${it.message}")
+                val builder = AlertDialog.Builder(this)
+                builder.setMessage(it.message).setTitle("Error")
+                val dialog = builder.create()
+                dialog.show()
                 return@exceptionally null
             }
+    }
+
+    private fun addToScene(fragment: ArFragment, anchor: Anchor, renderable: Renderable) {
+        val anchorNode = AnchorNode(anchor)
+
+        val skeletonNode = SkeletonNode()
+        skeletonNode.renderable = renderable
+
+        val node = TransformableNode(fragment.transformationSystem)
+        node.addChild(skeletonNode)
+        node.setParent(anchorNode)
+
+        fragment.arSceneView.scene.addChild(anchorNode)
     }
 }
